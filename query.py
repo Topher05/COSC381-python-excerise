2b072d8f2e8a7ba9a2d0e240227dba81e951433d
Topher05L <89708367+Topher05@users.noreply.github.com>
2023-11-01 10:22:43 -0400

    added the menu function to query.py
    issue #1

--- query.py
+++ query.py
@@ -1,3 +1,30 @@
 from movies import Movies
 
 movies = Movies('./movies.txt')
+
+def search_movie(search):
+    print(search)
+
+def search_cast(search):
+    print(search)
+
+def list_movies():
+    print('movies')
+
+while True:
+    print("q: quit \nsn: search movie names \nsc: search casts \nlist: prints all movie names \n: ")
+    userInput = input()
+    if userInput == 'q':
+        print('Have a nice day')
+        break
+    elif userInput == 'sn':
+        searchTerm = input('Search Movie: ')
+        search_movie(searchTerm)
+    elif userInput == 'sc':
+        castSearch = input('Enter cast member: ')
+        search_cast(castSearch)
+    elif userInput == 'list':
+        list_movies()
+    else :
+        print('Incorrect input\n')
+
